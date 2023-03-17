fbcde84e1e707050c7a0b971d964e600b52d0a09
Kim Jongyeob <zxch34@gmail.com>
2023-03-17 12:40:42 +0900

    update hello.py from yeob

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 # 천하제일 코드 자랑
 print("Hello, World!") # 전준형
+
+def jong_gang():  # 김종엽
+    
+    import datetime
+    
+    now =  datetime.datetime.now()
+    if now.day == 21:
+        print("종강!")
